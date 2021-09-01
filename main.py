17254de2e4eb868db7a05a410ed353d9d948b8b5
HolyMountain <yundead@yandex.ru>
2021-09-01 13:30:13 +0300

    Залив текущей версии в Гит

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import entering
 import logic
-
+# расчет типа загрузки по результатам анализа воды
 values = entering.values_input()
 logic.parameter_exception(values)
 result = logic.parameter_calculation(values)
